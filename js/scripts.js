ce3af6d04c88cdcc32a38894b091c7ce3d2d1c38
Dave Berning <daveberning@icloud.com>
2019-04-28 20:43:15 -0400

    wrapped up initial development

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
+var h = document.querySelector("header");
+var stuck = false;
+var stickPoint = getDistance();
+
+function getDistance() {
+  var topDist = h.offsetTop;
+  return topDist;
+}
+
+window.onscroll = function(e) {
+  var distance = getDistance() - window.pageYOffset;
+  var offset = window.pageYOffset;
+
+  if ( (distance <= 0) && !stuck) {
+    h.style.position = 'fixed';
+    h.style.top = '0px';
+    h.style.width = '100%';
+    stuck = true;
+  } else if (stuck && (offset <= stickPoint)){
+    h.style.position = 'static';
+    stuck = false;
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   var el = document.querySelector('.learn ul li')
   var info = document.querySelector('.learn p')
